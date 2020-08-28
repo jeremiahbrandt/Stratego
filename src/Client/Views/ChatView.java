3fb2b6beb260718bbea8aeb3b7ef56f8c1a6bce0
= <jeremiahjbrandt@outlook.com>
2020-08-28 19:56:33 +0000

    Finished layout of ChatView.java.

--- src/Client/Views/ChatView.java
+++ src/Client/Views/ChatView.java
@@ -1,25 +1,47 @@
 package Client.Views;
 
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class ChatView extends VBox {
+    private static Font titleFont = new Font(25);
+    private static Font secondaryFont = new Font(20);
+
     private Text title;
     private TextArea messages;
+
+    private HBox inputContainer;
     private TextField input;
     private Button sendButton;
 
+
     public ChatView() {
-        title = new Text("Messages");
-        TextArea textArea = new TextArea();
+        super.setAlignment(Pos.CENTER);
+        super.setPrefWidth(450);
+        title = new Text( "Messages");
+        title.setFont(titleFont);
+        messages = new TextArea();
+        messages.setEditable(false);
+        messages.setPrefHeight(685);
 
-        TextArea messages = new TextArea();
-        TextField input = new TextField();
+        inputContainer = new HBox();
+        inputContainer.setPrefHeight(50);
+        input = new TextField();
+        input.setFont(secondaryFont);
+        input.setPrefWidth(350);
         sendButton = new Button("Send");
+        sendButton.setPrefWidth(100);
+        sendButton.setFont(secondaryFont);
+        inputContainer.getChildren().addAll(input, sendButton);
 
-        super.getChildren().addAll(title, messages, input, sendButton);
+        super.getChildren().addAll(title, messages, inputContainer);
     }
 }
